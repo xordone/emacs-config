038056a9f471bbd378d9abfd8c387b28fc86296c
xordone <xordone@yandex.ru>
2021-12-07 16:13:16 +0300

    Вынес всё что касается режима зла, в отдельный файл. Пока рано с ним работать

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -82,34 +82,14 @@
 ;; general
 (use-package general
   :config
-  (general-create-definer rune/leader-keys
+  (general-create-definer pxl/leader-keys
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
     :global-prefix "M-SPC")
 
-  (rune/leader-keys
-    "f"  '(:ignore f :which-key "toggles")
-    "fn" '(counsel-load-theme :which-key "choose theme")))
-;; evil
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (pxl/leader-keys
+    "t"  '(:ignore f :which-key "test")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
 
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
+;;(when window-system
+;;  (load "~/.emacs.d/config/evil.el"))
